9026ef26cb1802dfc0b0f1c0dd897f8aabc70474
Mike McClenaghan <mike@sideline.ca>
2013-02-13 18:56:23 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem "middleman", "~> 3.0.11"
+gem "middleman-blog", "~> 3.1.1"
+gem "middleman-sync", "~> 3.0.8"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0.0"
+
+gem 'foreman'
---
+++ config.rb
@@ -0,0 +1,125 @@
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  set :site_url, ENV['SITE_URL']
+  set :site_title, ENV['SITE_TITLE']
+  set :author, ENV['AUTHOR']
+  set :description, ENV['DESCRIPTION']
+  set :gravatar_url, ENV['GRAVATAR_URL']
+  set :github, ENV['GITHUB']
+  set :twitter, ENV['TWITTER']
+    
+  # blog.prefix = "blog"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  blog.sources = "posts/:year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+# 
+# With no layout
+# page "/path/to/file.html", :layout => false
+# 
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+# 
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Enable cache buster
+  # activate :cache_buster
+  activate :asset_hash
+  
+  # Use relative URLs
+  # activate :relative_assets
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
+
+# Activate sync extension
+activate :sync do |sync|
+  sync.fog_provider = 'AWS'
+  sync.fog_directory = ENV['BUCKET']
+  sync.fog_region = 'us-east-1'
+  sync.aws_access_key_id = ENV['AWS_ACCESS_KEY']
+  sync.aws_secret_access_key = ENV['AWS_SECRET_KEY']
+  sync.existing_remote_files = 'keep'
+  # sync.gzip_compression = false
+  sync.after_build = true
+end
